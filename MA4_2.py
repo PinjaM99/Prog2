37db1b139a9278da7f452f97582c310003ab9e28
Pinja Moshiri <pimo7410@arrhenius.it.uu.se>
2021-10-14 15:27:32 +0200

    la till fib

--- MA4_2.py
+++ MA4_2.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 from integer import Integer
+from time import perf_counter as pc
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return(fib_py(n-1) + fib_py(n-2))
+
+
+
 
 def main():
 	f = Integer(5)
@@ -10,3 +24,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
