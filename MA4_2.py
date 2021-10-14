411d4cd54e0aa40b898066c2c17af7149b25d335
Pinja Moshiri <pimo7410@arrhenius.it.uu.se>
2021-10-14 16:47:22 +0200

    la till massa skit

--- MA4_2.py
+++ MA4_2.py
@@ -21,13 +21,41 @@ def main():
 	print(f.get())
 	f.set(7)
 	print(f.get())
-        start = pc()
-	print(fib_py(7))
-	stop = pc()
-	print(fib_py(f'{stop-start}'))
 
 
+        array = np.arange(25, 40)
+	time_py = []
+	time_cpp = []
 
+        for i in array:
+		start1 = pc()
+		fib_py(i)
+		stop1 = pc()
+		time_py.append(stop1-start1)
+
+        for j in array:
+		start2 = pc()
+		g = Integer(j)
+		g.fibcpp()
+		stop2 = pc()
+		time_cpp.append(stop2-start2)
+
+        plt.plot(array, time_py, 'r_', array, time_cpp, 'b-')
+	plt.show()
+	plt.savefig('Comparing fib 25 to 40 calculated with python and c++')
+
+        startpy = pc()
+	resultpy = fib_py(7)
+	print(resultpy)
+	stoppy = pc()
+	
+	startcpp = pc()
+	resultcpp = f.fibcpp()
+	print(resultcpp)
+	stopcpp = pc()
+
+	print(f'Calculating fib with python took {round(stoppy-startpy, 5)} seconds')
+	print(f'Calculating fib with C++ took {round(stopcpp-startcpp, 5)} seconds')
 
 if __name__ == '__main__':
 	main()
