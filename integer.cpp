4f5e8bf4974aa5946018096ba75e4bbe541bc7b5
Pinja Moshiri <pimo7410@arrhenius.it.uu.se>
2021-11-01 21:30:45 +0100

    tog bort 47

--- integer.cpp
+++ integer.cpp
@@ -27,7 +27,7 @@ void Integer::set(int n){
 int Integer::fibcpp(){
 	return fibcpp_private(val);
 }
-int Integer::fibcpp_private(int 47){
+int Integer::fibcpp_private(int n){
 	if(n<=1){
 		return n;
 	}
